147de52c6c743fa7110f1ac1334f1c71d2d514f2
ba0hai <ba0hai@protonmail.com>
2022-12-05 20:17:00 +0000

    initial commit

---
+++ days/day1/day1.py
@@ -0,0 +1,36 @@
+import numpy as np
+import heapq
+
+def get_input():
+    with open('input') as f:
+        lines = []
+        line = []
+        vals = f.readlines()
+        for val in vals:
+            if val != '\n':
+                val_len = len(val)
+                val = val[:val_len-1]
+                line.append(int(val))
+            else:
+                lines.append(line)
+                line = []
+    return lines
+
+def solve():
+    input = get_input()
+    sums = []
+    largest_3 = []
+
+    for list in input:
+        np_list = np.asarray(list)
+        summ = np.sum(np_list)
+        heapq.heappush(sums, summ)
+
+    return sum(heapq.nlargest(3, sums))
+
+def main():
+    print("Day 1 answer here")
+    ans = solve()
+    print(ans)
+
+main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+Some simple attempts at solving the exercises given during the Advent of Code 2022. Work is done in Python3. 
